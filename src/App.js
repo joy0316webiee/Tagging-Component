9276beba75f2d1103f2f0f044a41d81afccd7f0c
joy0316webiee <joy0316webiee@gmail.com>
2019-06-10 07:53:55 +1000

    build skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Tagging } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Tagging />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tagging/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Tagging extends Component {
+  render() {
+    return <div>I am happy</div>;
+  }
+}
+
+export { Tagging };
